6d3d8ed28a70af4c3717efa4d99bbb6d848999ad
EthanRicher <edrichards34@gmail.com>
2026-03-13 12:47:12 +1000

    Milestone 1

--- Todo_App/App.js
+++ Todo_App/App.js
@@ -1,6 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 
 export default function App() {
+
+   const Current_Tasks = [
+    { id: 1, Text: "Eggs" },
+    { id: 2, Text: "Milk" },
+    { id: 3, Text: "Ham" },
+    { id: 4, Text: "Chicken" }
+  ]
   return (
     <View style={styles.Container}>
 
@@ -14,16 +21,35 @@ export default function App() {
 
 
 
-      <View style={styles.Middle_Space}>
-        <Text style={styles.Main_Text}>Stuff</Text>
-      </View>
+    <View style={styles.Middle_Space}>
+
+
+        <View style={styles.List_Box}> 
+
+          {Current_Tasks.map(Task => (
+          <View key={Task.id} style={styles.Task_Box}> 
+            <Text style={styles.Task_Text}>{Task.id}. {Task.Text}</Text>
+          </View>
+          ))}
+
+        </View>  
+
+
+    </View>
 
 
 
 
-      <View style={styles.Footer_Space}>
-        <Text style={styles.Main_Text}>Add Task</Text>
-      </View>
+  <View style={styles.Footer_Space}>
+
+      <TouchableOpacity style={styles.Button_Box} onPress={() => console.log("Ping")}>
+        <Text style={styles.Button_Text}>Add Task</Text>
+      </TouchableOpacity>
+
+  </View>
+
+
+
 
     </View>
   )
@@ -73,11 +99,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 
-//-------------------------TEXT----------------------------------
+
+
+//-------------------------GENERIC TEXT----------------------------------
 //---------------------------------------------------------------
 
   Title_Text: {
-    color: "#dad7cd",
+    color: "black",
     fontSize: 35,
     fontWeight: "bold", 
   },
@@ -89,10 +117,57 @@ const styles = StyleSheet.create({
 
 
 
-
 //-------------------------COMPONENTS----------------------------------
+//---------------------------------------------------------------
 //---------------------------------------------------------------
 
 
 
+//-------------------------TASK BUTTON----------------------------------
+
+
+
+Button_Box: {
+    backgroundColor: "#a3b18a",
+    padding: 10,
+    borderRadius: 10,
+    width: "90%",
+    marginBottom: 10,
+    alignItems: "center",
+  },
+
+Button_Text: {
+    color: "black",
+    fontSize: 30,
+    fontWeight: "bold", 
+  },
+
+
+
+
+
+//-------------------------LIST----------------------------------
+  List_Box: {
+    backgroundColor: "#a3b18a50",
+    padding: 10,
+    height: "100%",
+    width: "90%",
+    alignItems: "center",
+  },
+  
+
+Task_Box: {
+    backgroundColor: "#344e41",
+    padding: 10,
+    borderRadius: 10,
+    width: "90%",
+    marginBottom: 20,
+    alignItems: "left",
+  },
+
+
+  Task_Text: {
+    color: "#dad7cd",
+    fontSize: 30,
+  },
 })
